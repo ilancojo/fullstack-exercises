6e99e0ea48a4697ea126b2bf0cc20192876b0313
ilan cojocaru <117562862+ilancojo@users.noreply.github.com>
2026-06-01 22:52:24 +0300

    finish exercise 2  - objectss

---
+++ pre-course/javascript/objects/Exercise2.js
@@ -0,0 +1,31 @@
+
+console.log("Exercise 2:");
+
+{
+  const library = {
+    books: [
+      {
+        title: "Harry Potter",
+        author: "J.K. Rowling"
+      },
+      {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien"
+      },
+      {
+        title: "1984",
+        author: "George Orwell"
+      },
+      {
+        title: "The Alchemist",
+        author: "Paulo Coelho"
+      },
+      {
+        title: "Atomic Habits",
+        author: "James Clear"
+      }
+    ]
+  };
+
+  console.log(library);
+}
