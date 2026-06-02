551968af7a80856952b0b2874e675c23b5c2a670
ilan cojocaru <117562862+ilancojo@users.noreply.github.com>
2026-06-02 14:49:26 +0300

    finish exercise1 functions

---
+++ pre-course/javascript/functions/Exercise1.js
@@ -0,0 +1,13 @@
+
+console.log("Exercise 1:");
+
+let number1 = 5;
+let number2 = 8;
+
+function isEvan(num){
+    
+    return console.log((num % 2 == 0) ? true : false);
+
+}
+isEvan(number1);
+isEvan(number2);
--- pre-course/javascript/loops/Exercise2.js
+++ pre-course/javascript/loops/Exercise2.js
@@ -7,7 +7,6 @@ console.log("Exercise 2:");
     { name: "Lucas", age: 18 }
   ];
 
-  // Print each person's name and age
   for (let i = 0; i < people.length; i++) {
     console.log(`${people[i].name} is ${people[i].age} years old`);
   }
