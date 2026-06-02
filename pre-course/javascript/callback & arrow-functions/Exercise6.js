325878f6aa323588cb021f306d043c99feeced8e
ilan cojocaru <117562862+ilancojo@users.noreply.github.com>
2026-06-03 01:11:01 +0300

    finish exercise 6

---
+++ pre-course/javascript/callback & arrow-functions/Exercise6.js
@@ -0,0 +1,16 @@
+
+
+console.log("Exercise 6:");
+
+const determineWeather = temp => {
+  if (temp > 25) {
+    return "hot";
+  }
+
+  return "cold";
+};
+
+const commentOnWeather = temp => "It's " + determineWeather(temp);
+
+console.log(commentOnWeather(30)); // It's hot
+console.log(commentOnWeather(22)); // It's cold
