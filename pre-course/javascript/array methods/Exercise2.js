884bc11ef3e9d530cd6e53f6c994ff3d112fa39d
ilan cojocaru <117562862+ilancojo@users.noreply.github.com>
2026-06-03 20:54:38 +0300

    finish exercise 2 - array methods

---
+++ pre-course/javascript/array methods/Exercise2.js
@@ -0,0 +1,22 @@
+console.log("Exercise 2:");
+
+/* 
+Exercise 2
+Create an array with user objects that have a zip-code that starts with "5".
+
+You should be left with the objects whose IDs are 3, 4, and 7.
+*/ 
+
+console.log("Exercise 2:")
+
+let userZip = users
+  .filter(user => user.address.zipcode[0] === "5")
+  .map(user => {
+    return {
+      id: user.id,
+      zipcode: user.address.zipcode
+    }
+  })
+
+console.log(userZip)
+
