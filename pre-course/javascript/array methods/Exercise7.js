424b873631d6a14b16eee4e398f881aaad44582f
ilan cojocaru <117562862+ilancojo@users.noreply.github.com>
2026-06-04 17:24:56 +0300

    finishing exercise 7 - array methods

--- pre-course/javascript/array methods/Exercise7.js
+++ pre-course/javascript/array methods/Exercise7.js
@@ -5,16 +5,17 @@ function findUserCityAndCompany(user){
   
   let newUserInfo = user.map(user => {
     return {  name: user.name,
-              address: user.city,    
+              city: user.address.city,    
               company : user.company.name };
     });
 
+    return newUserInfo;
 }
 
 findUserCityAndCompany(users).forEach(users => {
 
-  console.log( users.name + "is liveing in" + 
-    users.address.city + "and owns the company " + users.company );
+  console.log( users.name + "is liveing in " + 
+    users.city + " and owns the company : " + users.company );
 });
 
 
