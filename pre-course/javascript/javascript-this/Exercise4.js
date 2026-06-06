31ff724ce24eebd03ae6ed2d90612b09b65ce0ff
ilan cojocaru <117562862+ilancojo@users.noreply.github.com>
2026-06-06 18:14:06 +0300

    finishing this exercise 4

---
+++ pre-course/javascript/javascript-this/Exercise4.js
@@ -0,0 +1,26 @@
+
+console.log("Exercise 4:");
+
+const tipJar = {
+  coinCount: 20,
+   stealCoins: function (num) {
+    this.coinCount -= num;
+  },
+ 
+  tip: function () {
+    this.coinCount += 1;
+  }
+};
+
+
+
+
+tipJar.tip();
+console.log('Tip jar should have 21 coins: ' + tipJar.coinCount);
+
+tipJar.stealCoins(3);
+console.log('Tip jar should have 18 coins: ' + tipJar.coinCount);
+
+tipJar.stealCoins(10);
+console.log('Tip jar should have 8 coins: ' + tipJar.coinCount);
+
