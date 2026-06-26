837e7c39de3c87fce883e0c6eb9df23b7e3d429b
ilan cojocaru <117562862+ilancojo@users.noreply.github.com>
2026-06-26 22:50:47 +0300

    Starting the first exercise in react - buliding templet

---
+++ react/Intro-JSX/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  let companies = [
+    { name: "Tesla", revenue: 140 },
+    { name: "Microsoft", revenue: 300 },
+    { name: "Google", revenue: 600 }
+  ];
+
+  const showCompany = (name, revenue) => {
+  }
+
+  return (
+    <div className="ex-space">
+      <h4 className='ex-title'>Exercise 1</h4>
+      <div className="exercise" id="ex-1">
+        {}
+      </div>
+    </div>
+  )
+}
+
+export default App;
