b25fe912a9c4924c00e39e6c67647c7ac3313852
ilan cojocaru <117562862+ilancojo@users.noreply.github.com>
2026-06-27 01:19:15 +0300

    finish exe2

--- react/Intro-JSX/src/App.jsx
+++ react/Intro-JSX/src/App.jsx
@@ -11,17 +11,41 @@ function App() {
 const showCompany = (name, revenue) => {
   return (
     <div id={name}>
-      {name} makes {revenue} every year 
+       {name} makes {revenue} every year 
     </div>
   );
 }
 
+const getClassName = (temperature) => { 
+
+    const answer = (temperature >= 15 && temperature <= 30) ? "fair" : (temperature < 15) ? "freezing" : "hell-scape";    
+    return answer;
+}
+
+
+
   return (
     <div>
-      <h1>Exercise 1</h1>
-       {companies.map(company => showCompany(company.name, company.revenue))}
+      <div className="ex-space">
+        <h4 className='ex-title'>Exercise 1</h4>
+        <div className="exercise" id="ex-1">
+          {companies.map(company =>(
+            <div key = {company.name}>
+            {showCompany(company.name, company.revenue)}
+            </div>
+          ))} 
+        </div>
+      </div>
+
+      <div className="ex-space">
+        <h4 className='ex-title'>Exercise 2</h4>
+        <div className="exercise" id="ex-2">
+          <div id="weatherBox" className={getClassName(40)}>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
-
 export default App;
