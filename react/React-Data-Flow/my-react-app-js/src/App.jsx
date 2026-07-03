8d2f77c4e3da6675983bd178f346cfbe4a60ebf7
ilan cojocaru <117562862+ilancojo@users.noreply.github.com>
2026-07-03 19:37:17 +0300

    starting data flow exercise2

--- react/React-Data-Flow/my-react-app-js/src/App.jsx
+++ react/React-Data-Flow/my-react-app-js/src/App.jsx
@@ -1,11 +1,22 @@
 import Exercise1 from "./Exercise1/Exercise1";
+import Exercise2 from "./Exercise2-7/Exercise2";
 
+/*
 function App() {
   return (
     <div>
       <h1>Exercise 1</h1>
       <Exercise1 />
-    </div>
+   </div>
+  );
+}
+*/
+function App() {
+  return (
+    <div>
+      <h1>Exercise 2 </h1>
+      <Exercise2 />
+   </div>
   );
 }
 
--- react/React-Data-Flow/my-react-app-js/src/Exercise2-7/Exercise2.jsx
+++ react/React-Data-Flow/my-react-app-js/src/Exercise2-7/Exercise2.jsx
@@ -2,64 +2,70 @@
 import { useState } from "react";
 
 
-function Exercise1() {
 
-  const [gallery, setGallery] = useState({
-    images: [
-      "https://hips.hearstapps.com/hmg-prod/images/lychee-fruit-sugar-1530136136.jpg?crop=1xw:1xh;center,top&resize=640:*",
-      "https://hips.hearstapps.com/hmg-prod/images/mango-fruit-sugar-1530136260.jpg?crop=1xw:1xh;center,top&resize=640:*",
-      "https://hips.hearstapps.com/hmg-prod/images/cherries-sugar-fruit-1530136329.jpg?crop=1xw:1xh;center,top&resize=640:*",
-    ],
-    currentImg: 0,
+function List(){
+  return(
+    <div>
+      List
+    </div>
+  )
+}
+
+function Conversation(){
+  return(
+    <div>
+      Conversation
+    </div>
+  )
+}
+
+
+function Exercise2() {
+
+  const [chatState, setChatState ] = useState(
+  {
+      displayConversation: null,  //if null display conversations
+      conversations: [
+          {
+              with: "Laura", convo: [
+                  { text: "Hi", sender: "self" },
+                  { text: "You there?", sender: "self" },
+                  { text: "Yeah, hi, what's up?", sender: "other" }
+              ]
+          },
+          {
+              with: "Dad", convo: [
+                  { text: "Have you finished your school work yet?", sender: "other" },
+                  { text: "Yes.", sender: "self" },
+                  { text: "What do you mean, yes?", sender: "other" },
+                  { text: "??", sender: "self" }
+              ]
+          },
+          {
+              with: "Shoobert", convo: [
+                  { text: "Shoobert!!!", sender: "self" },
+                  { text: "Dude!!!!!!!!", sender: "other" },
+                  { text: "Shooooooooo BERT!", sender: "self" },
+                  { text: "You're my best friend", sender: "other" },
+                  { text: "No, *you're* my best friend", sender: "self" },
+              ]
+          }
+      ]
   });
 
 
-  function shiftImageBack() {
-    
-        setGallery((prevGallery) => (
-            {...prevGallery,
-            currentImg: prevGallery.currentImg - 1,
-  // אם נרצה לעשות בסיבוב את התמונות           
-//currentImg:(prevGallery.currentImg - 1 + prevGallery.images.length) %prevGallery.images.length          
-        }));
-  }
 
-    function shiftImageForward() {
-    
-        setGallery((prevGallery) => ({
-        ...prevGallery,
-        currentImg: prevGallery.currentImg + 1,
-  // אם נרצה לעשות בסיבוב את התמונות       
- //currentImg: (prevGallery.currentImg + 1) % prevGallery.images.length,
-       
-
-         }));
-  }
 
 
 
   return (
     <div>             
-        <button className="back" onClick={shiftImageBack}>
-            Back
-        </button>
-
-        <img  /*src של תמונה צריך לקבל מחרוזת של כתובת תמונה, לא פונקציה */
-            src={gallery.images[gallery.currentImg]}
-            alt="fruit"
-        />
-
-        <button className="forward" onClick={shiftImageForward}>
-            Forward
-         </button>
+        <div >
+          {chatState.displayConversation === null ? <List /> : <Conversation />}
+        </div>
     </div>
   );
 }
 
-export default Exercise1;
+export default Exercise2;
 
-//גישה נוספת היא לעצור את התמונות שלא יזוזו אחרי האינדקסים
-//BACK 
-//<button disabled={gallery.currentImg === 0}>
-//Forward
-//<button disabled={gallery.currentImg === gallery.images.length - 1}></button>
