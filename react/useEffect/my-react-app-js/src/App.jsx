b9961e24c1e9f67e4a5da6c6abc98aeba384150f
ilan cojocaru <117562862+ilancojo@users.noreply.github.com>
2026-07-06 02:12:20 +0300

    exercise 3 - useEffect - finish it

--- react/useEffect/my-react-app-js/src/App.jsx
+++ react/useEffect/my-react-app-js/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import  Clock  from './clock.jsx'
+import Posts from './post-api-exercise2.jsx'
 
 function App() {
 
   return (
     <div>
       <Clock></Clock>
+      <Posts></Posts>
     </div>
 
   )
--- react/useEffect/my-react-app-js/src/post-api-exercise2.jsx
+++ react/useEffect/my-react-app-js/src/post-api-exercise2.jsx
@@ -3,27 +3,31 @@ import { useEffect, useState } from "react"
 function Posts() {
   const [posts, setPosts] = useState([])
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(response => {
-        // להפוך ל-json
-      })
-      .then(data => {
-        // לקחת רק 10
-        // לעדכן state
-      })
-  }, [])
 
+  useEffect(() => { 
+    console.log("Effect started")
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {        // להפוך ל-json
+        //const data = response.json; 
+        return response.json()//אם לא עושים return, ה־.then הבא מקבל undefined.
+    })
+      .then(data => {
+         console.log("DATA:",data.slice(0, 10))
+        setPosts(data.slice(0, 10) )  ;// לקחת רק 10 // לעדכן state
+        
+    })
+  }, [])
+ console.log("Post render:", posts)
   return (
     <div>
       <h1>Top Posts</h1>
 
-      <div>
+      <div className="posts-container">
         {
           posts.map(post => (
-            <div key={post.id}>
-              {/* title */}
-              {/* body */}
+            <div className="post-card" key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
             </div>
           ))
         }
