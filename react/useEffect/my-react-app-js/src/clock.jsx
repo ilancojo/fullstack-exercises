e02523322a71235e142d88907d4d88cb16f6c5b7
ilan cojocaru <117562862+ilancojo@users.noreply.github.com>
2026-07-06 00:56:35 +0300

    exercise 1 - useEffect - finish it

--- react/useEffect/my-react-app-js/src/clock.jsx
+++ react/useEffect/my-react-app-js/src/clock.jsx
@@ -1,23 +1,28 @@
 import { useEffect, useState } from "react"
 
-function Clock() {
-  const [currentTime, setCurrentTime] = useState(null)
+function Clock() {                      //useState(null)
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString())//להתחיל מהזמן הנוכחי -כדי שלא יהיה רגע ריק במסך
 
   useEffect(() => {
+    //2. console.log("Effect started")
     const intervalId = setInterval(() => {
-      
+        //4. console.log("Tick:", new Date().toLocaleTimeString())
         // עדכן את currentTime לזמן חדש
-        currentTime = new Date().toLocaleTimeString();
-        setCurrentTime(currentTime);
+        setCurrentTime(new Date().toLocaleTimeString())
+        //רעיונית React לא רוצה שנשנה state ישירות
+        //currentTime = new Date().toLocaleTimeString();
+        //setCurrentTime(currentTime);
     }, 1000);
 
 
     return () => {
+      //3.  console.log("Cleanup interval:", intervalId)
       // נקה את intervalId
         clearInterval(intervalId)
     }
   }, [])
 
+    //1. console.log("Clock render:", currentTime)
   return (
     <div>
       {currentTime}
