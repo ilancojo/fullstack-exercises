ae49ff3fbff8afa5f9ed9dbb7d7fd2253f55e604
ilan cojocaru <117562862+ilancojo@users.noreply.github.com>
2026-07-06 02:32:58 +0300

    exercise 3 - useEffect - adding Mobile css

--- react/useEffect/my-react-app-js/src/post-api-exercise2.jsx
+++ react/useEffect/my-react-app-js/src/post-api-exercise2.jsx
@@ -1,23 +1,40 @@
 import { useEffect, useState } from "react"
 
 function Posts() {
-  const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([])
+    const [isSmallScreen, setIsSmallScreen] = useState(false)
 
 
-  useEffect(() => { 
-    console.log("Effect started")
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(response => {        // להפוך ל-json
-        //const data = response.json; 
-        return response.json()//אם לא עושים return, ה־.then הבא מקבל undefined.
-    })
-      .then(data => {
-         console.log("DATA:",data.slice(0, 10))
-        setPosts(data.slice(0, 10) )  ;// לקחת רק 10 // לעדכן state
-        
-    })
-  }, [])
- console.log("Post render:", posts)
+    useEffect(() => { 
+        console.log("Effect started")
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => {        // להפוך ל-json
+            //const data = response.json; 
+            return response.json()//אם לא עושים return, ה־.then הבא מקבל undefined.
+        })
+        .then(data => {
+            console.log("DATA:",data.slice(0, 10))
+            setPosts(data.slice(0, 10) )  ;// לקחת רק 10 // לעדכן state
+            
+        })
+    }, [])
+
+    useEffect(() => {
+        function handleResize() {
+            // בדוק window.innerWidth
+            setIsSmallScreen(true);
+        }
+
+        handleResize()
+
+        window.addEventListener("resize", handleResize)
+
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
+        }, [])
+
+
   return (
     <div>
       <h1>Top Posts</h1>
