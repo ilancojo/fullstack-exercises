6b09461203719dd3b9f16931f0d97fe8e4ac187a
ilan cojocaru <117562862+ilancojo@users.noreply.github.com>
2026-08-07 18:58:32 +0300

    fix  exercise 2

--- backend/furniture-store/server.js
+++ backend/furniture-store/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const http = require("http");
-
 const app = express();
+const path = require("path");
+
 const port = "8080";
 /*
  app.listen(port, function(){
@@ -9,7 +10,16 @@ const port = "8080";
 })
  עד לפה שרת מינימלי*/
 
+//המידע
+const store = [
+    { name: "table", inventory: 3, price: 800 },
+    { name: "chair", inventory: 16, price: 120 },
+    { name: "couch", inventory: 1, price: 1200 },
+    { name: "picture frame", inventory: 31, price: 70 }
+]
 
+app.use(express.static(path.join(__dirname,"dist")));    
+app.use(express.static(path.join(__dirname,"node_modules")));    
 
 app.get("/",(req,res)=>{
     res.send("Server is up and running smoothly");
@@ -19,12 +29,18 @@ app.get("/buy/:name",(req,res)=>{
     res.send("Server is up and running smoothly");
 });
 
-app.get("/buy/:name",(req,res)=>{
-    res.send("Server is up and running smoothly");
-});
                     //paramter - name
 app.get("/priceCheck/:name",(req,res)=>{
-    res.send("Server is up and running smoothly");
+
+    const itemName = req.params.name
+    const item = store.find(furniture => furniture.name === itemName)
+
+    if (item) {
+        res.send({ price: item.price })
+    } else {
+        res.send({ price: null })
+    }
+
 });
 
 
@@ -32,9 +48,3 @@ app.listen(port, function(){
     console.log(`Running server on port ${port}`)
 })
 
-const store = [
-    { name: "table", inventory: 3, price: 800 },
-    { name: "chair", inventory: 16, price: 120 },
-    { name: "couch", inventory: 1, price: 1200 },
-    { name: "picture frame", inventory: 31, price: 70 }
-]
